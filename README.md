b4b2d0a5a68401a48adadd556a756edc1d0b5a3c
lordsarcastic <>
2024-01-05 15:19:45 +0100

    Feature: Implemented script to ingest ECR images

--- README.md
+++ README.md
@@ -1,2 +1,197 @@
 # example-ecr-images
 Python Script for Ingesting ECR Images in Port
+
+## Getting started
+In this example, you will create blueprints for `ecrImage` and `ecrRepository` that ingests images and repositories from ECR into Port. You will then use a Python script to make API calls to AWS ECR REST API to fetch the data from your account.
+
+### Blueprints
+Create the following blueprints in Port using the schemas:
+
+
+#### Repository
+```json
+{
+  "identifier": "ecrRepository",
+  "description": "This blueprint represents an ECR Repository",
+  "title": "ECR Repository",
+  "icon": "AWS",
+  "schema": {
+    "properties": {
+        "name": {
+            "type": "string",
+            "title": "Repository Name",
+            "description": "The name of the repository",
+        },
+        "registryId": {
+            "type": "string",
+            "title": "Registry ID",
+            "description": "The ID of the registry",
+        },
+      "arn": {
+        "type": "string",
+        "title": "Repository ARN",
+        "description": "The ARN of the repository",
+      },
+      "uri": {
+        "type": "string",
+        "title": "Repository URI",
+        "description": "The URI of the repository",
+      },
+        "createdAt": {
+            "type": "string",
+            "title": "Created At",
+            "description": "Date and time the repository was created",
+            "format": "date-time"
+        },
+      "imageTagMutability": {
+        "type": "string",
+        "title": "Image Tag Mutability",
+        "description": "The image tag mutability setting for the repository",
+        "enum": [
+          "MUTABLE",
+          "IMMUTABLE"
+        ],
+        "enumColors": {
+            "MUTABLE": "green",
+            "IMMUTABLE": "darkGray"
+        }
+      },
+      "configurationScanOnPush": {
+        "type": "boolean",
+        "title": "Configuration Scan on Push",
+        "description": "Image scanning configuration when pushing images to this repository",
+      },
+      "encryptionType": {
+        "type": "string",
+        "title": "Encryption Type",
+        "description": "The encryption type of the repository",
+        "enum": [
+          "AES256",
+          "KMS"
+        ],
+        "enumColors": {
+            "AES256": "green",
+            "KMS": "blue"
+        }
+      },
+      "kmsKey": {
+        "type": "string",
+        "title": "KMS Key",
+        "description": "The KMS key used for encryption",
+      }
+    },
+    "required": []
+  },
+  "mirrorProperties": {},
+  "calculationProperties": {},
+  "aggregationProperties": {},
+  "relations": {}
+}
+```
+
+#### Image
+```json
+{
+  "identifier": "ecrImage",
+  "description": "This blueprint represents an ECR image",
+  "title": "ECR Image",
+  "icon": "AWS",
+  "schema": {
+    "properties": {
+      "registryId": {
+        "type": "string",
+        "title": "Registry ID",
+        "description": "The ID of the registry"
+      },
+      "digest": {
+        "type": "string",
+        "title": "Image Digest",
+        "description": "SHA256 digest of image manifest"
+      },
+      "tags": {
+        "type": "array",
+        "title": "Image Tags",
+        "description": "List of tags for the image"
+      },
+      "size": {
+        "type": "number",
+        "title": "Image Size",
+        "description": "Size of the image in bytes"
+      },
+      "pushedAt": {
+        "type": "string",
+        "title": "Pushed At",
+        "description": "Date and time the image was pushed to the repository",
+        "format": "date-time"
+      },
+      "manifestMediaType": {
+        "type": "string",
+        "title": "Manifest Media Type",
+        "description": "The media type of the image manifest"
+      },
+      "artifactMediaType": {
+        "type": "string",
+        "title": "Artifact Media Type",
+        "description": "The media type of the image artifact"
+      },
+      "lastRecordedPullTime": {
+        "type": "string",
+        "title": "Last Recorded Pull Time",
+        "description": "Date and time the image was last pulled",
+        "format": "date-time"
+      }
+    },
+    "required": []
+  },
+  "mirrorProperties": {},
+  "calculationProperties": {},
+  "aggregationProperties": {},
+  "relations": {
+    "repository": {
+      "title": "ECR Repository",
+      "description": "Repository of the image",
+      "target": "ecrRepository",
+      "required": true,
+      "many": false
+    }
+  }
+}
+```
+
+### Running the Python script
+First clone the repository and cd into the work directory with:
+```bash
+$ git clone git@github.com:port-labs/example-ecr-images.git
+$ cd example-ecr-images
+```
+
+Install the needed dependencies within the context of a virtual environment with:
+```bash
+$ virtualenv venv
+$ source venv/bin/activate
+$ pip3 install -r requirements.txt
+```
+
+To ingest your data, you need to populate some environment variables. You can do that by either duplicating the `.example.env` file and renaming the copy as `.env`, then edit the values as needed; or run the commands below in your terminal:
+
+```bash
+export PORT_CLIENT_ID=port_client_id
+export PORT_CLIENT_SECRET=port_client_secret
+export AWS_ACCESS_KEY_ID=aws_access_key_id
+export AWS_SECRET_ACCESS_KEY=aws_secret_access_key
+export AWS_DEFAULT_REGION=aws_default_region
+export AWS_REGISTRY_ID=aws_account_id
+```
+
+Then run the script with:
+```bash
+$ python app.py
+```
+
+Each variable required are:
+- PORT_CLIENT_ID: Port Client ID
+- PORT_CLIENT_SECRET: Port Client secret
+- AWS_ACCESS_KEY_ID: AWS access key ID
+- AWS_SECRET_ACCESS_KEY: AWS secret access key
+- AWS_DEFAULT_REGION: AWS default region where the ECR repository is located
+- AWS_REGISTRY_ID: AWS account ID
---
+++ app.py
@@ -0,0 +1,210 @@
+import asyncio
+import logging
+import os
+from typing import Any
+
+import aiohttp
+import boto3
+import dotenv
+import requests
+
+dotenv.load_dotenv()
+
+logger = logging.getLogger(__name__)
+
+PORT_API_URL = "https://api.getport.io/v1"
+PORT_CLIENT_ID = os.getenv("PORT_CLIENT_ID")
+PORT_CLIENT_SECRET = os.getenv("PORT_CLIENT_SECRET")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
+AWS_REGISTRY_ID = os.getenv("AWS_REGISTRY_ID")
+
+ECR_REPOSITORY_BLUEPRINT = "ecrRepository"
+ECR_IMAGE_BLUEPRINT = "ecrImage"
+MAX_RESULTS = 999
+
+
+# Get Port Access Token
+credentials = {"clientId": PORT_CLIENT_ID, "clientSecret": PORT_CLIENT_SECRET}
+token_response = requests.post(f"{PORT_API_URL}/auth/access_token", json=credentials)
+access_token = token_response.json()["accessToken"]
+
+# You can now use the value in access_token when making further requests
+headers = {"Authorization": f"Bearer {access_token}"}
+
+
+async def add_entity_to_port(
+    session: aiohttp.ClientSession, blueprint_id, entity_object
+):
+    """A function to create the passed entity in Port
+
+    Params
+    --------------
+    blueprint_id: str
+        The blueprint id to create the entity in Port
+
+    entity_object: dict
+        The entity to add in your Port catalog
+
+    Returns
+    --------------
+    response: dict
+        The response object after calling the webhook
+    """
+    logger.info(f"Adding entity to Port: {entity_object}")
+    response = await session.post(
+        (
+            f"{PORT_API_URL}/blueprints/"
+            f"{blueprint_id}/entities?upsert=true&merge=true"
+        ),
+        json=entity_object,
+        headers=headers,
+    )
+    if not response.ok:
+        logger.info("Ingesting {blueprint_id} entity to port failed, skipping...")
+    logger.info(f"Added entity to Port: {entity_object}")
+
+
+client = boto3.client("ecr", region_name=AWS_DEFAULT_REGION)
+
+
+async def get_all_repositories(session: aiohttp.ClientSession):
+    """A function to get all repositories from ECR
+
+    Returns
+    --------------
+    repositories: list
+        A list of all repositories in an ECR registry
+    """
+    next_token = None
+    kwargs = {}
+    logger.info("Getting all repositories from ECR")
+
+    while True:
+        if next_token:
+            kwargs = {"nextToken": next_token}
+
+        response = client.describe_repositories(
+            registryId=AWS_REGISTRY_ID,
+            maxResults=MAX_RESULTS,
+            **kwargs,
+        )
+        yield response["repositories"]
+        next_token = response.get("nextToken")
+        if not next_token:
+            break
+    logger.info(f"Got all repositories from ECR registry: {AWS_REGISTRY_ID}")
+
+
+async def get_all_images(session: aiohttp.ClientSession, repository_name):
+    """A function to get all images from a repository in ECR
+
+    Params
+    --------------
+    repository_name: str
+        The name of the repository to get images from
+
+    Returns
+    --------------
+    images: list
+        A list of all images in a repository
+    """
+    next_token = None
+    kwargs = {}
+    logger.info(f"Getting all images from ECR repository: {repository_name}")
+
+    while True:
+        if next_token:
+            kwargs = {"nextToken": next_token}
+
+        response = client.describe_images(
+            registryId=AWS_REGISTRY_ID,
+            repositoryName=repository_name,
+            maxResults=MAX_RESULTS,
+            **kwargs,
+        )
+        yield response["imageDetails"]
+        next_token = response.get("nextToken")
+        if not next_token:
+            break
+
+    logger.info(f"Got all images from ECR repository: {repository_name}")
+
+
+async def ingest_ecr_repositories(
+    session: aiohttp.ClientSession, repository: dict[str, Any]
+):
+    encryption_type = None
+    kms_key = None
+    encryption_configuration = repository.get("encryptionConfiguration")
+
+    if encryption_configuration:
+        encryption_type = encryption_configuration.get("encryptionType")
+        kms_key = encryption_configuration.get("kmsKey")
+
+    repository_object = {
+        "name": repository["repositoryName"],
+        "registryId": repository["registryId"],
+        "arn": repository["repositoryArn"],
+        "uri": repository["repositoryUri"],
+        "createdAt": repository["createdAt"].strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "imageTagMutability": repository["imageTagMutability"],
+        "configurationScanOnPush": repository["imageScanningConfiguration"][
+            "scanOnPush"
+        ],
+        "encryptionType": encryption_type,
+        "kmsKey": kms_key,
+    }
+    entity_object = {
+        "identifier": repository_object["name"],
+        "title": repository_object["name"],
+        "properties": {**repository_object},
+    }
+
+    await add_entity_to_port(session, ECR_REPOSITORY_BLUEPRINT, entity_object)
+
+
+async def ingest_ecr_images(session: aiohttp.ClientSession, image: dict[str, Any]):
+    last_recorded_pull_time = image.get("lastRecordedPullTime")
+    if last_recorded_pull_time:
+        last_recorded_pull_time = last_recorded_pull_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    entity_object = {
+        "identifier": image["imageDigest"],
+        "title": image["imageDigest"],
+        "properties": {
+            "registryId": image["registryId"],
+            "digest": image["imageDigest"],
+            "tags": image["imageTags"],
+            "size": image["imageSizeInBytes"],
+            "pushedAt": image["imagePushedAt"].strftime("%Y-%m-%dT%H:%M:%SZ"),
+            "manifestMediaType": image["imageManifestMediaType"],
+            "artifactMediaType": image["artifactMediaType"],
+            "lastRecordedPullTime": last_recorded_pull_time,
+        },
+        "relations": {
+            "repository": image["repositoryName"],
+        },
+    }
+    await add_entity_to_port(session, ECR_IMAGE_BLUEPRINT, entity_object)
+
+
+async def main():
+    logger.info("Starting Port integration")
+    async with aiohttp.ClientSession() as session:
+        async for repositories in get_all_repositories(session):
+            for repository in repositories:
+                await ingest_ecr_repositories(session, repository)
+
+                async for images in get_all_images(
+                    session, repository["repositoryName"]
+                ):
+                    for image in images:
+                        await ingest_ecr_images(session, image)
+
+    logger.info("Finished Port integration")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
